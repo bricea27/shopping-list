3546b884c86789a3c3e60fef9cd40da4352da1d2
Andrew Brice <andrew.brice@outreach.io>
2023-12-21 14:43:40 -0500

    feat: add basic search functionalilty

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import { useState } from 'react'
+
 import './App.css'
 
 function App() {
+  const [searchResults, setSearchResults] = useState<string[]>([]);
+
+  const handleSearch = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const query = event?.target?.value ?? '';
+    const response = await fetch(`https://api.frontendeval.com/fake/food/${query}`)
+    setSearchResults(await response.json());
+  }
 
   return (
     <main>
@@ -8,6 +17,12 @@ function App() {
         <h1>Shop-it</h1>
         <p>Search for an item, add items, check them off, and delete them!</p>
       </header>
+      <section>
+        <input onChange={handleSearch} type="text" placeholder="Search for an item" />
+        <ul>
+        {searchResults.map(result => <li>{result}</li>)}
+        </ul>
+      </section>
     </main>
   )
 }
